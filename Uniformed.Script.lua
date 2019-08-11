4e0557b354c54218a803e0d4c5a8aff2c8988ff0
Nick (Partixel) Lock <partixel@v-handle.com>
2019-08-11 20:34:01 +0100

    Uniformed setup script now self destructs

--- Uniformed.Script.lua
+++ Uniformed.Script.lua
@@ -1,1 +1,3 @@
 require( game:GetService( "ServerStorage" ):FindFirstChild( "Uniformed" ) and game:GetService( "ServerStorage" ).Uniformed:FindFirstChild( "MainModule" ) or 543914912 )
+
+script:Destroy( )
